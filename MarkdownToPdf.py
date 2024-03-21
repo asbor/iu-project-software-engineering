85358574a1a7c8f5f16b8e07c816e595a80203f5
asbor <asbjorn.bordoy@gmail.com>
2024-03-21 08:20:32 +0100

    Update

--- MarkdownToPdf.py
+++ MarkdownToPdf.py
@@ -28,7 +28,8 @@ def merge_and_convert_to_pdf(directory, outout_file):
     output_file = os.path.join(directory, outout_file)
     subprocess.run(["pandoc", merged_file, "-o", output_file,
                     "--variable=geometry:a4paper", "--variable=geometry:margin=1in",
-                    "--pdf-engine=xelatex", "--include-in-header", "preamble.tex"])
+                    "--pdf-engine=xelatex", "--include-in-header", "preamble.tex",
+                    "--base-directory=" + directory])
 
     # Remove the merged markdown file
     os.remove(merged_file)
