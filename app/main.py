705b600d31484bf89291a8ca18e2d5b64513d20d
asbor <asbjorn.bordoy@gmail.com>
2024-04-06 19:27:47 +0200

    New  project created in the web directory

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ Date:           2024-apr-04
 from model import db_models
 from model.database import engine
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware  # NEW
 from app.controller.routes.router import router
 
 # create the database tables if they do not exist already
@@ -19,6 +20,16 @@ app = FastAPI(title="PostgreSQL and FastAPI")
 app.include_router(router)
 
 # create a root endpoint
+
+# NEW
+# CORSMiddleware is required to make cross-origin requests -- i.e., requests that originate from a different protocol, IP address, domain name, or port. This is necessary since the frontend will run at http://localhost:8080.
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
