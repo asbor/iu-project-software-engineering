69d5c776081bbd3c596cf0c7229a03f0771f4a95
Asbjorn Bordoy <asbjorn.bordoy@gmail.com>
2024-06-05 14:14:19 +0200

    Update

--- services/backend/tests/test_endpoints/test_questions.py
+++ services/backend/tests/test_endpoints/test_questions.py
@@ -64,8 +64,8 @@ def test_create_and_get_all_questions():
     response = client.get("/questions")
     assert (
         response.status_code == 200
-    ), f"Unexpected status code: {response.status_code},
-    response: {response.json()}"
+    ), f'''Unexpected status code: {response.status_code},
+    response: {response.json()}'''
 
     assert len(response.json()) == 1
     question = response.json()[0]
@@ -93,8 +93,8 @@ def test_delete_question():
     )
     assert (
         response.status_code == 200
-    ), f"Unexpected status code: {response.status_code},
-    response: {response.json()}"
+    ), f'''Unexpected status code: {response.status_code},
+    response: {response.json()}'''
 
     question_id = response.json()["id"]
     # Delete the question
@@ -102,15 +102,15 @@ def test_delete_question():
     response = client.delete(f"/questions/{question_id}")
     assert (
         response.status_code == 200
-    ), f"Unexpected status code: {response.status_code},
-    response: {response.json()}"
+    ), f'''Unexpected status code: {response.status_code},
+    response: {response.json()}'''
 
     # Ensure the question is deleted
 
     response = client.get("/questions")
     assert (
         response.status_code == 200
-    ), f"Unexpected status code: {response.status_code},
-    response: {response.json()}"
+    ), f'''Unexpected status code: {response.status_code},
+    response: {response.json()}'''
 
     assert not any(q["id"] == question_id for q in response.json())
